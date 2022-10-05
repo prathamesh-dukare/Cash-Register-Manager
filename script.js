80a39c09b1ca79f3b5f659b34c69e1cd32cfca49
Prathamesh Dukare <Prathameshdukaremail@gmail.com>
2022-10-05 23:39:09 +0530

    remove console logs

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ let notesArray = [2000, 500, 200, 100, 50, 20, 10, 5, 1]
 // controller Funcions
 function showMessage(msg) { alert(msg); }
 function updateNotes(i, notes) {
-    // console.log('from updateNotes', notesArray[i], '-----', notes, 'index', i)//debug
     notesCount[i].innerText = notes
 }
 
@@ -19,13 +18,13 @@ function checkHandler() {
         if (parseInt(cashGiven.value) > 0) {
             if (parseInt(cashGiven.value) >= parseInt(billAmount.value)) {
                 let RemainingAmount = parseInt(cashGiven.value) - parseInt(billAmount.value);
-                // console.log(RemainingAmount, 'initial Remaining Value!')//Debug
+
                 for (let i = 0; i < notesArray.length; i++) {
                     if (RemainingAmount !== 0) {
                         let notes = Math.trunc(RemainingAmount / notesArray[i])//returns integer value after divide
                         updateNotes(i, notes)
                         RemainingAmount = RemainingAmount % notesArray[i]
-                        console.log('new remaining amount', RemainingAmount)
+
                     } else { updateNotes(i, 0) }
                 }
             } else { showMessage(`Given Amount is less than Bill Amount! No change to be return.Instead you need to take ${parseInt(billAmount.value) - parseInt(cashGiven.value)} RS from Customer`) }
